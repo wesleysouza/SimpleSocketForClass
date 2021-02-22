8357a1a825238041691e06e3292a24c1c09d8af4
wesleysouza <wsouzacs@gmail.com>
2021-02-21 22:09:24 -0300

    trocando porta

--- simple_socket_client.py
+++ simple_socket_client.py
@@ -9,4 +9,6 @@ def socket_client_tcp(host, port):
 	print('Received', repr(data))
 
 #EC2 Machine IP
-socket_client_tcp('35.153.194.255', 250)
+#socket_client_tcp('35.153.194.255', 250)
+socket_client_tcp("127.0.0.1", 250)
+#socket_client_tcp("192.168.56.104", 65001)
