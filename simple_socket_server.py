623089a04e6462c910bcca709e19b3a28121a512
wesleysouza <wsouzacs@gmail.com>
2021-02-21 21:03:36 -0300

    add socket client and server

---
+++ simple_socket_server.py
@@ -0,0 +1,19 @@
+import socket
+
+def socket_server_tcp(host, port):
+	socket_server_tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	socket_server_tcp.bind(host, port)
+	socket_server_tcp.listen(1)
+	conn, addr = socket_server_tcp.accept()
+	print('NEW SOCKET', conn)
+	print('Connected by', addr)
+	while True:
+		data = conn.recv(1024)
+		if not data:
+			break
+		else:
+			print("Dados redebidos!!")
+			print(data)
+			conn.send(data)
+
+socket_server_tcp('', 50007)
